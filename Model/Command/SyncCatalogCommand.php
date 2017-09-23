f37287537b56714b241419e4af67e4b19947db2d
Echron <stijn@echron.be>
2017-09-23 20:41:12 +0200

    Use resource to communicate with API

--- Model/Command/SyncCatalogCommand.php
+++ Model/Command/SyncCatalogCommand.php
@@ -3,18 +3,19 @@ declare(strict_types=1);
 
 namespace Attlaz\Base\Model\Command;
 
+use Attlaz\Base\Model\Resource\BaseResource;
+
 class SyncCatalogCommand
 {
+    private $resource;
+
+    public function __construct(BaseResource $baseResource)
+    {
+        $this->resource = $baseResource;
+    }
     public function syncCatalog(array $externalIds, bool $skipImages): array
     {
-        $endPoint = '';
-        $clientId = '';
-        $clientSecret = '';
-        $accessToken = '';
-        $client = new \Attlaz\Client($endPoint);
-        $client->setCredentials($clientId, $clientSecret, $accessToken);
-        $client->setBranch('espa');
-        $result = $client->scheduleTask('syncCatalog', [
+        $result = $this->resource->scheduleTask('syncCatalog', [
             'externalIds' => $externalIds,
             'skipImages'  => $skipImages,
         ]);
